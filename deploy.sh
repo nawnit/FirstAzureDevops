f33b1eec329f64156235bd7f55eed88d6537cd27
nawnit <nawnit86@gmail.com>
2020-04-02 01:09:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ elif  [ $ENVIRONMENT = "PROD" ];then
 				sshpass -p "1234" scp target/gamutkart.war nawnit@172.17.0.4:/home/nawnit/Distros/apache-tomcat-8.5.53/webapps
 				sshpass -p "1234" ssh nawnit@172.17.0.4 "JAVA_HOME=/home/nawnit/Distros/jdk1.8.0_241" "/home/nawnit/Distros/apache-tomcat-8.5.53/bin/startup.sh"
 elif  [ $ENVIRONMENT = "ec2-tomcatserver" ];then				
-	 sshpass -p "deployer" ssh  deployer@3.22.57.181 "JAVA_HOME=/usr/bin/java" "/opt/apache-tomcat-8.5.53/bin/shutdown.sh"
+	                  ssh -i "mykey.pem" ubuntu@ec2-3-22-57-181.us-east-2.compute.amazonaws.com "JAVA_HOME=/usr/bin/java" "/opt/apache-tomcat-8.5.53/bin/shutdown.sh"
 				print "PROD deployment is done!"
-				sshpass -p deployer ssh deployer@3.22.57.181 "JAVA_HOME=/usr/bin/java" "rm /opt/apache-tomcat-8.5.53/webapps/gamutkart.war"
-				sshpass -p "deployer" scp target/gamutkart.war deployer@3.22.57.181:/opt/apache-tomcat-8.5.53/webapps
-				sshpass -p "deployer" deployer@3.22.57.181 "JAVA_HOME=/usr/bin/java" "/opt/apache-tomcat-8.5.53/bin/startup.sh"
+				ssh -i "mykey.pem" ubuntu@ec2-3-22-57-181.us-east-2.compute.amazonaws.com "JAVA_HOME=/usr/bin/java" "rm /opt/apache-tomcat-8.5.53/webapps/gamutkart.war"
+				scp -i "mykey.pem" target/gamutkart.war ubuntu@ec2-3-22-57-181.us-east-2.compute.amazonaws.com:/opt/apache-tomcat-8.5.53/webapps
+				ssh -i "mykey.pem" ubuntu@ec2-3-22-57-181.us-east-2.compute.amazonaws.com "JAVA_HOME=/usr/bin/java" "/opt/apache-tomcat-8.5.53/bin/startup.sh"
 echo "deployment has been done!"
 fi
